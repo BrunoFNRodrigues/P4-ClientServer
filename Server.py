ca9cab7cef0076c7b8425f3b8bd476fe74b5d973
keiyanishio <keiyan@al.insper.edu.br>
2021-09-24 16:29:25 -0300

    servi a dor

--- Server.py
+++ Server.py
@@ -15,35 +15,42 @@ def main():
         #aqui você deverá gerar os dados a serem transmitidos. 
         imageW = "./imgs/recebidaCopia.png"
         SaveImage = open(imageW, 'wb')
+        ocioso = True 
 
-        BufferRx, nrx = com4.getData(15)
-        print(BufferRx)
-        txLen = BufferRx[10:11]
-        time.sleep(0.01)
-        com4.sendData(np.asarray(txLen))
+        while ocioso == True:
+            msgt1, nrx = com4.getData(14, 1)
+            if msgt1[0:1] == b'/x01':
+                if msgt1[2:3] == b'/x0f':
+                    ocioso = False
+            time.sleep(1)
 
-        PacksLen = int.from_bytes(txLen, "big")-1
-        print("Recebendo dados...")
-        num_pack = -1
-        num_pack_v = 0
-        while num_pack != PacksLen:
-            head, nRx = com4.getData(10)
-            txLen = int.from_bytes(head[5:6], "big")
-            print("txLen",txLen)
-
-            package, nRx = com4.getData(txLen)
-            eop, nRx = com4.getData(4)
-            print("EOP", eop)
-            num_pack = int.from_bytes(head[4:5], "big")
-            print("Pacote {}/{} Enviado:".format(num_pack, PacksLen),package)
-            time.sleep(0.01)
-
-            if num_pack == num_pack_v and eop == b'\xde\xee\xeeU':
-                num_pack_v += 1
-                com4.sendData(np.asarray(Datagrama(tipo="data", payload=b'\x01')))
+        com4.sendData(np.asarray(Datagrama(tipo="2")))
+        cont = 1
+        while cont <= msgt1[4:5]:
+            start_timer1 = time.time()
+            start_timer2 = time.time()
+            msgt3, nrx = com4.getData(10)
+            payload, nrx = com4.getData(msgt3[6:7])
+            eop, nrx = com4.getData(4)
+            if msgt3[0:1] == b'/x03':
+                if num_pack == cont and eop == b'\xFF\xAA\xFF\xAA':
+                    cont += 1
+                    com4.sendData(np.asarray(Datagrama(tipo="4")))
+                    SaveImage.write(payload)
+                else:
+                    com4.sendData(np.asarray(Datagrama(tipo="6")))
             else:
-                com4.sendData(np.asarray(Datagrama(tipo="data", payload=b'\x00')))
-            SaveImage.write(package)
+                time.sleep(1)
+                if time.time()-start_timer2 > 20:
+                    ociosso = True
+                    com4.sendData(np.asarray(Datagrama(tipo="5")))
+                    print("( ͡ಥ ͜ʖ ͡ಥ)")
+                    com4.disable()
+                    exit()
+                else:
+                    if time.time()-start_timer1 > 2:
+                        com4.sendData(np.asarray(Datagrama(tipo="4")))
+                        start_timer1 = time.time()
 
         SaveImage.close()
     
